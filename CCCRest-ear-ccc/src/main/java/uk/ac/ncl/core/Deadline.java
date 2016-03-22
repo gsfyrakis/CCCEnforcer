6351b1a0ee2c0ef9f4e8b8cb792379ee742e3b17
Ioannis Sfyrakis <i.sfyrakis@ncl.ac.uk>
2016-03-22 13:50:28 +0000

    cleanup deadline Class

--- CCCRest-ear-ccc/src/main/java/uk/ac/ncl/core/Deadline.java
+++ CCCRest-ear-ccc/src/main/java/uk/ac/ncl/core/Deadline.java
@@ -1,47 +1,48 @@
 package uk.ac.ncl.core;
 
+import java.util.Date;
 import java.util.TimerTask;
-import javax.annotation.Resource;
 import javax.ejb.Stateful;
-import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.HeuristicMixedException;
-import javax.transaction.HeuristicRollbackException;
-import javax.transaction.NotSupportedException;
-import javax.transaction.RollbackException;
-import javax.transaction.SystemException;
-import javax.transaction.TransactionManager;
 import javax.transaction.UserTransaction;
 
 import org.jboss.tm.usertx.client.ServerVMClientUserTransaction;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.ac.ncl.resource.Resources;
-import uk.ac.ncl.rop.Obligation;
-import uk.ac.ncl.rop.Prohibition;
+import uk.ac.ncl.rop.DeadlineInt;
 import uk.ac.ncl.rop.RopEntity;
 import uk.ac.ncl.state.RopState;
-import uk.ac.ncl.state.RopState.ObligationState;
-import uk.ac.ncl.state.RopState.ProhibitionState;
-import uk.ac.ncl.user.User;
 
 @Stateful
 @Transactional
-public class Deadline extends TimerTask {
+public class Deadline extends TimerTask implements DeadlineInt{
+    private Date deadline;
     private RopEntity<? extends RopState> rop;
 
-    @Resource(mappedName = "java:jboss/TransactionManager")
-    private TransactionManager transactionManager;
+    private TimeKeeper timeKeeper = null;
+//   	private RopEntity rop = null;
+   	private String originator = null;
+   	private String responder = null;
+   	// Flag to identify if this deadline is for an expiry:
+   	// if true it is, otherwise it's a deadline for a timeout
+   	private boolean expiryFlag = true;
+
+
+//    @Resource(mappedName = "java:jboss/TransactionManager")
+//    private TransactionManager transactionManager;
 
     private UserTransaction userTransaction;
 
     @PersistenceContext(unitName = "RopePU")
     private EntityManager em;
+
+
+    public Deadline(Date deadline) {
+        this.deadline = deadline;
+    }
 
     public Deadline() {
         em = Resources.getEntityManager();
@@ -53,62 +54,43 @@ public class Deadline extends TimerTask {
         userTransaction = new ServerVMClientUserTransaction(com.arjuna.ats.jta.TransactionManager.transactionManager());
     }
 
+    public Deadline(TimeKeeper timeKeeper, RopEntity rop, String originator, String responder, boolean b) {
+        this.timeKeeper = timeKeeper;
+        this.rop = rop;
+        this.expiryFlag = expiryFlag;
+
+    }
+
+    public Deadline(TimeKeeper timeKeeper, RopEntity rop, String originator, String responder) {
+
+        this.timeKeeper = timeKeeper;
+        this.rop = rop;
+        this.expiryFlag = true;
+
+    }
+
     @Override
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void run() {
-        System.out.println("The deadline starts!");
-        try {
-            userTransaction.begin();
-        } catch (NotSupportedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (SystemException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        User user = new User("zhao", "belldandy", "PI", null, null, null);
-        em.persist(user);
-        em.flush();
-        System.out.println("The obligation: " + " has been expired!");
-        try {
-            userTransaction.commit();
-        } catch (SecurityException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IllegalStateException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (RollbackException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (HeuristicMixedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (HeuristicRollbackException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (SystemException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        /*
-		 * if (rop instanceof Obligation) { Obligation obligation = (Obligation)
-		 * rop; if (obligation.getState() == ObligationState.imposed) {
-		 * //Obligation obligationEntity = em.merge(obligation);
-		 * //obligationEntity.setState(ObligationState.violated);
-		 * //em.persist(obligation); User user = new User("zhao", "belldandy",
-		 * "PI", null, null, null); userManager.store(user);
-		 * System.out.println("The obligation: " + obligation.getName() +
-		 * " has been expired!"); } } else if (rop instanceof Prohibition) {
-		 * Prohibition prohibition = (Prohibition) rop; if
-		 * (prohibition.getState() == ProhibitionState.imposed) {
-		 * prohibition.setState(ProhibitionState.expired);
-		 * 
-		 * 
-		 * System.out.println("The prohibition: " + prohibition.getName() +
-		 * " has been expired!"); }
-		 * 
-		 * }
-		 */
+        timeKeeper.deadlineCallback(rop, originator, responder, expiryFlag);
+
     }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    /* Checks if is expiry.
+  	 *
+  	 * @return true, if is expiry
+  	 */
+  	public boolean isExpiry() {
+  		return expiryFlag;
+  	}
+
+
 }
