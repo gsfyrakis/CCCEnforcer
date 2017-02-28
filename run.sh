cccfdec47030cb39ddb4ad6c78b375eb27170dd2
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2017-02-28 00:32:34 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # run standalone Jboss 7.1.1
-exec $JBOSS_HOME/bin/standalone.sh  --server-config=standalone-full-ha.xml
+exec $JBOSS_HOME/bin/standalone.sh  
+#--server-config=standalone-full-ha.xml
